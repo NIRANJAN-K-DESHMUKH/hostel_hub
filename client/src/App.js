3bc7b9955d9d2ed14c4d5b42ba4df80160117b80
Niranjan K D <niranjandeshmukh371@gmail.com>
2024-02-01 22:06:02 +0530

    initial commit :)

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import LoginPage from './pages/login/LoginPage.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/login/LoginPage.jsx
@@ -0,0 +1,53 @@
+import "./login.css";
+import React, { useState } from 'react';
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add authentication logic here
+    if (username === "admin" && password ==="admin"){
+      console.log('Username:', username);
+      console.log('Password:', password);
+    } else {
+      console.log('Invalid');
+
+    }
+    
+  };
+  return (
+    <div className='login'>
+      <div className="loginWrapper">
+        <div className="leftDiv">
+          <img src="assets/landingPageImage.png" alt="" className="leftImg"/>
+        </div>
+        <div className="rightDiv">
+                    
+          <form onSubmit={handleSubmit}>
+          <h1>Hostel Hub</h1>
+            <label>Username</label>
+              <input type="text" value={username} onChange={handleUsernameChange} />
+            
+            <br /><br />
+            <label>Password</label>
+              <input type="password" value={password} onChange={handlePasswordChange} />
+            
+              <br /><br />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+        
+      </div>
+      
+    </div>
+    
+  );
+};
+export default LoginPage;
