e2ac0afad0527c16d92aa68ec506f66f1a37deda
Niranjan K D <niranjandeshmukh371@gmail.com>
2024-04-03 21:25:11 +0530

    update BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
+import { useEffect } from 'react';
 import DashboardPage from './pages/dashboard/DashboardPage.jsx';
 import LoginPage from './pages/login/LoginPage.jsx';
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  Navigate
 } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 const App = () => {
+  const loggedInUserData = useSelector((state) => {
+    return state.Reducers.user;
+  });
+  useEffect(()=>{
+    localStorage.setItem("user", JSON.stringify(loggedInUserData));
+  }, [loggedInUserData]);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<LoginPage />} />
-        {/* <Route path="/register" element={<RegisterPage />} /> */}
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route exact path="/" element={loggedInUserData ? <DashboardPage /> : <LoginPage />} />
+        <Route path="/login" element={loggedInUserData ? <Navigate to="/" /> : <LoginPage />} />
+        <Route path="/dashboard" element={loggedInUserData ? <DashboardPage /> : <LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
