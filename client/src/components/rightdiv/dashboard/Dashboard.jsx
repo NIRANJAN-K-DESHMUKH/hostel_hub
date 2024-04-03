0f7c2372313436e160d94d2b36851ca523eed331
Niranjan K D <niranjandeshmukh371@gmail.com>
2024-04-03 21:32:20 +0530

    fetch and display request log in dashboard

--- client/src/components/rightdiv/dashboard/Dashboard.jsx
+++ client/src/components/rightdiv/dashboard/Dashboard.jsx
@@ -1,24 +1,51 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import CleaningCard from "../cleaningEntry/CleaningCard";
+import CleaningCard from "../roomcleaning/cleaningEntry/CleaningCard";
+import ComplaintCard from "../complaint/complaintEntry/ComplaintCard";
+import { useSelector } from "react-redux";
 
 const Dashboard = (props) => {
     const toShow = props.show;
     const [cleaningReqs, setCleaningReqs] = useState([]);
-    const studentRegNo = "20BCT0220";
+    const [complaints, setComplaints] = useState([]);
+    const student = useSelector((state) => {
+      return state.Reducers.user;
+    });
+    const showComponents = useSelector((state) => {
+      return state.Reducers.showComponents;
+    });
     
     useEffect(() => {
       const fetchCleaningReqs = async () => {
-        try {
-          const res = await axios.post("/api/cleaningreq/all/" + studentRegNo, {isStudent: true});
-          // console.log("cleaningReqs");
+        if(student) {
+          try {
+            const res = await axios.post("/api/cleaningreq/all/" + student.studentRegNo, {isStudent: true});
+            // console.log("cleaningReqs");
+  
+            // setCleaningReqs(
+            //   res.data.sort((p1, p2) => {
+            //     return new Date(p2.createdAt) - new Date(p1.createdAt);
+            //   })
+            // );
+            setCleaningReqs(
+              res.data
+            );
+            // console.log(cleaningReqs);
+          } catch (error) {
+            console.log(error)
+          }
+        } else {
+          console.log("Student not signed in!");
+        }
+        
+      };
 
-          // setCleaningReqs(
-          //   res.data.sort((p1, p2) => {
-          //     return new Date(p2.createdAt) - new Date(p1.createdAt);
-          //   })
-          // );
-          setCleaningReqs(
+      const fetchComplaints = async () => {
+        try {
+          const res = await axios.post("/api/complaint/all/" + student.studentRegNo, {isStudent: true});
+              // console.log(res.data);
+
+          setComplaints(
             res.data
           );
           // console.log(cleaningReqs);
@@ -26,32 +53,11 @@ const Dashboard = (props) => {
           console.log(error)
         }
       };
-      fetchCleaningReqs();
-    },[]);
 
-    // const fetchCleaningReqs = async () => {
-    //   try {
-    //     const res = await axios.post("/api/cleaningreq/all/" + studentRegNo, {isStudent: true});
-    //     // const res = await axios(
-    //     //   {method: "get",
-    //     //   url: "/api/cleaningreq/all/20BCT0220",
-    //     //   data: [{
-    //     //     isStudent: "true",
-    //     //   }]
-    //     // });
-    //     // setCleaningReqs(
-    //     //   res.data.sort((p1, p2) => {
-    //     //     return new Date(p2.createdAt) - new Date(p1.createdAt);
-    //     //   })
-    //     // );
-    //     // setCleaningReqs(
-    //     //   res
-    //     // );
-    //     console.log(res);
-    //   } catch (error) {
-    //     console.log(error)
-    //   }
-    // };
+      fetchCleaningReqs();
+      fetchComplaints();
+
+    },[showComponents]);
 
     return (
       <>
@@ -59,11 +65,11 @@ const Dashboard = (props) => {
             ? <div className='dashboardwrapper'>
                  <h1 className='title'>Dashboard</h1>
                  <div>
-                    {cleaningReqs.map((c) => (
-                      <CleaningCard key={c[0]._id} regNo={c[0].studentRegNo} />
-                    ))}
+                      <CleaningCard cleaningreqs={JSON.stringify(cleaningReqs)} />
                  </div>
-                 <span>hello</span>
+                 <div>
+                      <ComplaintCard complaints={JSON.stringify(complaints)} />
+                 </div>
              </div> 
             : <></> }
       </>
