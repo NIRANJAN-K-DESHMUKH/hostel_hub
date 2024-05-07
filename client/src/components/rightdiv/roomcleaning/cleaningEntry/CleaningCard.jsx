c6cc6500124109e4f94c15d33bbda8c8af701937
Niranjan K D <niranjandeshmukh371@gmail.com>
2024-05-08 00:50:52 +0530

    add otp verification

--- client/src/components/rightdiv/roomcleaning/cleaningEntry/CleaningCard.jsx
+++ client/src/components/rightdiv/roomcleaning/cleaningEntry/CleaningCard.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import axios from "axios";
 
 const CleaningCard = (props) => {
     const cleaningreq = props.cleaningreq;
     const [otp, setotp] = useState("");
+    const [newW, setNewW] = useState(false);
 
 
     const checkOTP = async (event) => {
@@ -10,9 +12,18 @@ const CleaningCard = (props) => {
 
         if(cleaningreq.otp == otp) {
           console.log("correct otp");
-          await axios.put()
+          await axios.patch("/api/cleaningreq/"+cleaningreq.cleaningreqId,
+            {
+              isCompletedStatus: "true",
+              isStudent: true
+            }
+          );
+          setNewW(true);
+          alert("OTP Verified Successfully 😊\n\nRoom Cleaning Completed.");
+
         } else {
-          console.log("incorrect otp");
+          alert("Incorrect OTP 🙁\n\nVerification Failed.");
+
         }
     }
 
@@ -23,20 +34,22 @@ const CleaningCard = (props) => {
         
           <span className="headings">{cleaningreq.studentRegNo}</span>
       
-          <span className="headings">{cleaningreq.room_number}</span>
+          {/* <span className="headings">{cleaningreq.room_number}</span> */}
     
           <span className="studentComments">{cleaningreq.studentComments ? cleaningreq.studentComments : "-"}</span>
-      
-          <span className="headings">{cleaningreq.isCompletedStatus ? "true" : "false"}</span>
+        
+         {/* !newW ? <span className="headings">{!newW ? "true": "false"}</span>:<></> */}
+          <span className="headings">{cleaningreq.isCompletedStatus ? "true" : newW ? "true": "false" }</span>
 
           {/* <span className="headings">{cleaningreq.updatedAt.slice(0, 10)+" "+cleaningreq.updatedAt.slice(11, 13)}</span> */}
           <span className="headings">{new Date(cleaningreq.createdAt).toLocaleDateString()} {new Date(cleaningreq.createdAt).toLocaleTimeString()}</span>
 
           <span className="headings">{cleaningreq.isCompletedStatus ? "" : 
-          <form onSubmit={checkOTP}>
-            <input type="text" size="6" placeholder="Enter OTP" required value={otp} onChange={(e) => setotp(e.target.value)}/> 
-            <button type="submit">Submit</button>
-          </form> }
+          !newW ? <form onSubmit={checkOTP}>
+          <input type="text" size="6" placeholder="Enter OTP" required value={otp} onChange={(e) => setotp(e.target.value)}/> 
+          <button type="submit">Submit</button>
+        </form> : <></>
+           }
           </span>
 
         </div>
