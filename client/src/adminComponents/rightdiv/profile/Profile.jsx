dedb3b30e1427a43057ca722e5f48659cb0c8701
Niranjan K D <niranjandeshmukh371@gmail.com>
2024-05-08 00:38:14 +0530

    modify admin profile page

--- client/src/adminComponents/rightdiv/profile/Profile.jsx
+++ client/src/adminComponents/rightdiv/profile/Profile.jsx
@@ -14,11 +14,14 @@ const Profile = (props) => {
         { toShow 
             ? 
             <div>
-                <h1>admin profile page </h1>
+                <h1 className="adminHH">Admin Profile Page </h1>
+                <div className="admindddd">
                 <p name="studName" className='admindetails'>Name: {admin.adminName}</p>
                 <p name="studRoomNo" className='admindetails'>Hostel Block: {admin.hostelBlockName}</p>
                 <p name="studPhoneNo" className='admindetails'>Phone Number: {admin.adminPhone_no}</p>
-                <p name="studEmailId" className='admindetails'>Email: {admin.adminEmail}</p> 
+                <p name="studEmailId" className='admindetails'>Email: {admin.adminEmail}</p>
+                </div>
+                 
             </div>
             : <></>
         }
